5f4dc6aa3396168b158ee3dd5b5726bd65e0385d
katy <lozanokate5@gmail.com>
2021-03-24 17:32:42 -0600

    Este cambio es debido al dar click en el botón se pasa a un nuevo view llamado modalView, que contiene un saludo para el usuario

--- GitProject/ContentView.swift
+++ GitProject/ContentView.swift
@@ -16,8 +16,9 @@ struct ContentView: View {
             Button("Show Modal"){
                 self.isPresented = true
             }.sheet(isPresented: $isPresented){
-                Text("This is the modal screen")
+                ModalView()
             }
+            .navigationBarTitle("Xcode and Git")
         }
     }
 }
---
+++ GitProject/ModalView.swift
@@ -0,0 +1,21 @@
+//
+//  ModalView.swift
+//  GitProject
+//
+//  Created by catalina lozano on 24/03/21.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    
+    var body: some View {
+        Text("This is the modal View!")
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
