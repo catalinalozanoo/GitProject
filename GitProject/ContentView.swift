ba5ba065268183610255c27616b742df131296b4
katy <lozanokate5@gmail.com>
2021-03-24 18:59:22 -0600

    Se agrego VStack

--- GitProject/ContentView.swift
+++ GitProject/ContentView.swift
@@ -11,10 +11,13 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView{
+            VStack{
+                Text("This is the Content View")
             NavigationLink(destination: ModalView()){
                     Text("Go Next")
             }.foregroundColor(.purple)
             .navigationBarTitle("Xcode and Git")
+            }
         }
     }
 }
